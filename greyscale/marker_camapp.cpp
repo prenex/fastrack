c982da10f9d6f32687fb272c68edf3b718f7d2e0
Richard Thier <u9vata@gmail.com>
2018-05-31 14:36:20 +0200

    Removed GLUT and just using simple opengl now...

--- greyscale/marker_camapp.cpp
+++ greyscale/marker_camapp.cpp
@@ -1,322 +1,303 @@
-// Compile with: g++ marker_camapp.cpp -lglut -lGLESv2 --std=c++14 -o marker_camapp
+// Compile with: g++ marker_camapp.cpp -lGL -lX11 -o marker_camapp
 
-
-#define MCA_VERSION "0.1"
-
-// There is one file where we should do these
-#define GL_GLEXT_PROTOTYPES
-#define EGL_EGLEXT_PROTOTYPES
-
-//#define _GNU_SOURCE
-
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <Glut/glut.h>
-#else
-#include <GL/gl.h>
-#include <GL/glut.h>
-#endif
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <cctype>
 #include <sys/time.h>
-#include <unistd.h>
-#include <string>
+#define GL_GLEXT_PROTOTYPES
+#define GLX_GLXEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glx.h>
 
-#include "mathelper.h"
-#include "loghelper.h"
+struct MyWin {
+	Display  *display;
+	Window win;
+	int displayed;
+	int width;
+	int height;
+};
 
-#define DEBUG 1
-/* #define DEBUG_EXTRA */
+const int WIN_XPOS = 256;
+const int WIN_YPOS = 64;
+const int WIN_XRES = 640;
+const int WIN_YRES = 480;
+const int NUM_SAMPLES = 1;
 
-/* Randomly chosen default width/heigth */
-int win_width = 640;
-int win_heigth = 480;
+struct MyWin Win;
 
-/** The location of the shader uniforms */
-static GLuint ModelViewProjectionMatrix_location,
-	      NormalMatrix_location,
-	      LightSourcePosition_location,
-	      MaterialColor_location,
-	      TextureSampler_location;
-/** The projection matrix */
-static GLfloat ProjectionMatrix[16];
-/** The direction of the directional light for the scene */
-static const GLfloat LightSourcePosition[4] = { 5.0, 5.0, 10.0, 1.0};
-
-static void printGlError(std::string where) {
-	GLenum err = glGetError();
-	if(err != GL_NO_ERROR) {
-		LOGE((where + " - glError: 0x%x").c_str(), err);
-	}
+double elapsedMsec(const struct timeval *start, const struct timeval *stop) {
+	return ((stop->tv_sec  - start->tv_sec ) * 1000.0 +
+			(stop->tv_usec - start->tv_usec) / 1000.0);
 }
 
-/**
- * Main drawing routine
- */
-static void draw() {
-	const static GLfloat red[4] = { 0.8, 0.1, 0.0, 1.0 };
-	const static GLfloat green[4] = { 0.0, 0.8, 0.2, 1.0 };
-	const static GLfloat blue[4] = { 0.2, 0.2, 1.0, 1.0 };
-	GLfloat transform[16];
-	identity(transform);
-
-	// Cornflowerblue for having a retro feeling from my XNA years :-)
-	glClearColor(0.3921, 0.5843, 0.9294, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	/* Translate and rotate the view */
-	translate(transform, 0, 0, -40);
-	//rotate(transform, 2 * M_PI * view_rot[0] / 360.0, 1, 0, 0);
-	//rotate(transform, 2 * M_PI * view_rot[1] / 360.0, 0, 1, 0);
-	//rotate(transform, 2 * M_PI * view_rot[2] / 360.0, 0, 0, 1);
-
-	// TODO: Render the scene
-
-	// Swap the buffers and render everything
-	glutSwapBuffers();
-
-#ifdef LONGTEST
-	glutPostRedisplay(); // check for issues with not throttling calls
-#endif
-}
-
-/**
- * Handles a new window size or exposure.
- *
- * @param width the window width
- * @param height the window height
- */
-static void handleViewportReshape(int width, int height) {
-	/* Update the projection matrix */
-	perspective(ProjectionMatrix, 60.0, width / (float)height, 1.0, 64.0);
-
-	/* Set the viewport */
-	glViewport(0, 0, (GLint) width, (GLint) height);
-}
-
-/**
- * Handles special glut events.
- *
- * @param special the event to handle.
- * @param x the x coordinate of the mouse when the event happened
- * @param y the y coordinate of the mouse when the event happened
- */
-static void handleSpecialGlutEvents(int special, int x, int y) {
-	// TODO: do whatever
-	switch (special) {
-		case GLUT_KEY_LEFT:
-			break;
-		case GLUT_KEY_RIGHT:
-			break;
-		case GLUT_KEY_UP:
-			break;
-		case GLUT_KEY_DOWN:
-			break;
-		case GLUT_KEY_F11:
-			glutFullScreen();
-			break;
-	}
-}
-
-/**
- * Handles mouse-related glut events.
- *
- * @param button GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, or GLUT_RIGHT_BUTTON
- * @param state GLUT_UP or GLUT_DOWN indicating whether the callback was due to a release or press
- * @param x the x coordinate of the mouse when the event happened
- * @param y the y coordinate of the mouse when the event happened
- */
-static void handleMouseGlutEvents(int button, int state, int x, int y) {
-	LOGI("mouse event(%d,%d,x:%d,y:%d)", button, state, x, y);
-}
-
-static void idle(void) {
-	static int frames = 0;
-	static double tRot0 = -1.0, tRate0 = -1.0;
-	double dt, t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-
-	if (tRot0 < 0.0)
-		tRot0 = t;
-	dt = t - tRot0;
-	tRot0 = t;
-
-#ifdef TEST_MEMORYPROFILER_ALLOCATIONS_MAP
-	// This file is used to test --memoryprofiler linker flag, in which case
-	// add some allocation noise.
-	static void *allocatedPtr = 0;
-	free(allocatedPtr);
-	allocatedPtr = malloc(rand() % 10485760);
-#endif
-
-	glutPostRedisplay();
-	frames++;
-
-	if (tRate0 < 0.0)
-		tRate0 = t;
-	if (t - tRate0 >= 5.0) {
-		GLfloat seconds = t - tRate0;
-		GLfloat fps = frames / seconds;
-		printf("%d frames in %3.1f seconds = %6.3f FPS\n", frames, seconds,
-				fps);
-		tRate0 = t;
-		frames = 0;
-#ifdef LONGTEST
-		static runs = 0;
-		runs++;
-		if (runs == 4) {
-			int result = fps;
-#ifdef TEST_MEMORYPROFILER_ALLOCATIONS_MAP
-			result = 0;
-#endif
-			REPORT_RESULT();
+void draw() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	/*
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f( 0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glVertex3f( 1.0f, -1.0f, 0.0f);
+	glEnd();
+	*/
+	uint8_t col = rand() % 256;
+	uint8_t data[640 * 480];
+	for(size_t y = 0; y < WIN_YRES; ++y) {
+		for(size_t x = 0; x < WIN_XRES; ++x) {
+			data[x+y*WIN_XRES] = col;
 		}
-#endif
+	}
+
+	glDrawPixels(WIN_XRES, WIN_YRES, GL_LUMINANCE, GL_UNSIGNED_BYTE, data);
+	glFlush();
+	glXSwapBuffers(Win.display, Win.win);
+}
+
+void keyboardCB(KeySym sym, unsigned char key, int x, int y,
+		int *setting_change) {
+	switch (tolower(key)) {
+		case 27:
+			exit(EXIT_SUCCESS);
+			break;
+		case 'k':
+			printf("You hit the 'k' key\n");
+			break;
+		case 0:
+			switch (sym) {
+				case XK_Left  :
+					printf("You hit the Left Arrow key\n");
+					break;
+				case XK_Right :
+					printf("You hit the Right Arrow key\n");
+					break;
+			}
+			break;
 	}
 }
 
-// TODO: Change accordingly
-static const char vertex_shader[] =
-"attribute vec3 position;\n"
-"attribute vec3 normal;\n"
-"attribute vec2 texcoord;\n"
-"\n"
-"uniform mat4 ModelViewProjectionMatrix;\n"
-"uniform mat4 NormalMatrix;\n"
-"uniform vec4 LightSourcePosition;\n"
-"uniform vec4 MaterialColor;\n"
-"\n"
-"varying vec4 Color;\n"
-"varying vec2 fragTex;\n"
-"\n"
-"void main(void)\n"
-"{\n"
-"    // Transform the normal to eye coordinates\n"
-"    vec3 N = normalize(vec3(NormalMatrix * vec4(normal, 1.0)));\n"
-"\n"
-"    // The LightSourcePosition is actually its direction for directional light\n"
-"    vec3 L = normalize(LightSourcePosition.xyz);\n"
-"\n"
-"    // Multiply the diffuse value by the vertex color (which is fixed in this case)\n"
-"    // to get the actual color that we will use to draw this vertex with\n"
-"    float diffuse = max(dot(N, L), 0.0);\n"
-"    Color = diffuse * MaterialColor;\n"
-"\n"
-"    // Transform the position to clip coordinates\n"
-"    gl_Position = ModelViewProjectionMatrix * vec4(position, 1.0);\n"
-"    // Fill the varying textcoord for the fragment shader\n"
-"    fragTex = texcoord;\n"
-"}";
+void reshapeCB(int width, int height) {
+	Win.width = width;
+	Win.height = height;
+	glViewport(0, 0, width, height);
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+	//glMatrixMode(GL_MODELVIEW);
+}
 
-static const char fragment_shader[] =
-"#ifdef GL_ES\n"
-"precision mediump float;\n"
-"#endif\n"
-"varying vec4 Color;\n"
-"varying vec2 fragTex;\n"
-"uniform sampler2D texSampler2D;\n"
-"\n"
-"void main(void)\n"
-"{\n"
-"    vec4 texel = texture2D(texSampler2D, fragTex);\n"
-//"    gl_FragColor = vec4(Color.rgb * texel.rgb, texel.a);\n"
-//"    gl_FragColor = Color;\n"
-"    gl_FragColor = texel;\n"
-// For testing texture indices
-//"    gl_FragColor = (Color + texel) + vec4(fragTex, 0.5, 1.0);\n"
-"}";
+/* Try to find a framebuffer config that matches
+ * the specified pixel requirements.
+ */
+GLXFBConfig chooseFBConfig(Display *display, int screen) {
+	static const int Visual_attribs[] = {
+		GLX_X_RENDERABLE  , True,
+		GLX_DRAWABLE_TYPE , GLX_WINDOW_BIT,
+		GLX_RENDER_TYPE   , GLX_RGBA_BIT,
+		GLX_X_VISUAL_TYPE , GLX_TRUE_COLOR,
+		GLX_RED_SIZE	  , 8,
+		GLX_GREEN_SIZE	  , 8,
+		GLX_BLUE_SIZE	  , 8,
+		GLX_ALPHA_SIZE	  , 8,
+		GLX_DEPTH_SIZE	  , 16,
+		GLX_STENCIL_SIZE  , 8,
+		GLX_DOUBLEBUFFER  , True,
+		GLX_SAMPLE_BUFFERS, 1,
+		GLX_SAMPLES		  , 4,
+		None
+	};
+	int attribs [ 100 ] ;
+	memcpy(attribs, Visual_attribs, sizeof(Visual_attribs));
+	GLXFBConfig ret = 0;
+	int fbcount;
+	GLXFBConfig *fbc = glXChooseFBConfig(display, screen, attribs, &fbcount);
+	if (fbc) {
+		if (fbcount >= 1)
+			ret = fbc[0];
+		XFree(fbc);
+	}
+	return ret;
+}
 
-static void init(char* modelFileNameAndPath) {
-	GLuint v, f, program;
-	const char *p;
-	char msg[512];
+GLXContext createContext(Display *display, int screen,
+		GLXFBConfig fbconfig, XVisualInfo *visinfo, Window window) {
+#define GLX_CONTEXT_MAJOR_VERSION_ARB		0x2091
+#define GLX_CONTEXT_MINOR_VERSION_ARB		0x2092
+	typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*,
+			GLXFBConfig, GLXContext, int, const int*);
+	/* Verify GL driver supports glXCreateContextAttribsARB() */
+	/*	 Create an old-style GLX context first, to get the correct function ptr. */
+	glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
+	GLXContext ctx_old = glXCreateContext(display, visinfo, 0, True);
+	if (!ctx_old) {
+		printf("Could not even allocate an old-style GL context!\n");
+		exit(EXIT_FAILURE);
+	}
+	glXMakeCurrent (display, window, ctx_old) ;
+	/* Verify that GLX implementation supports the new context create call */
+	if (strstr(glXQueryExtensionsString(display, screen),
+				"GLX_ARB_create_context") != 0)
+		glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)
+			glXGetProcAddress((const GLubyte *) "glXCreateContextAttribsARB");
+	if (!glXCreateContextAttribsARB) {
+		printf("Can't create new-style GL context\n");
+		exit(EXIT_FAILURE);
+	}
+	/* Got the pointer.  Nuke old context. */
+	glXMakeCurrent(display, None, 0);
+	glXDestroyContext(display, ctx_old);
 
-	glDisable(GL_CULL_FACE);
-	glEnable(GL_DEPTH_TEST);
+	/* Try to allocate a GL 4.2 COMPATIBILITY context */
+	static int Context_attribs[] = {
+		GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
+		GLX_CONTEXT_MINOR_VERSION_ARB, 2,
+		GLX_CONTEXT_PROFILE_MASK_ARB , GLX_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+		/*GLX_CONTEXT_PROFILE_MASK_ARB, GLX_CONTEXT_CORE_PROFILE_BIT_ARB, */
+		/*GLX_CONTEXT_FLAGS_ARB		  , GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB, */
+		/*GLX_CONTEXT_FLAGS_ARB		  , GLX_CONTEXT_DEBUG_BIT_ARB, */
+		None
+	};
+	GLXContext context = glXCreateContextAttribsARB(display, fbconfig, 0,
+			True, Context_attribs);
+	/* Forcably wait on any resulting X errors */
+	XSync(display, False);
+	if (!context) {
+		printf("Failed to allocate a GL 4.2 context\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("Created GL 4.2 context\n");
+	return context;
+}
 
-	/* Compile the vertex shader */
-	p = vertex_shader;
-	v = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(v, 1, &p, NULL);
-	glCompileShader(v);
-	glGetShaderInfoLog(v, sizeof msg, NULL, msg);
-	printf("vertex shader info: %s\n", msg);
+void createWindow() {
+	/* Init X and GLX */
+	Win.displayed = 0;
+	Display *display = Win.display = XOpenDisplay(":0.0");
+	if (!display)
+		printf("Cannot open X display\n");
+	int    screen	= DefaultScreen(display);
+	Window root_win = RootWindow(display, screen);
+	if (!glXQueryExtension(display, 0, 0))
+		printf("X Server doesn't support GLX extension\n");
+	/* Pick an FBconfig and visual */
+	GLXFBConfig fbconfig = chooseFBConfig(display, screen);
+	if (!fbconfig) {
+		printf("Failed to get GLXFBConfig\n");
+		exit(EXIT_FAILURE);
+	}
+	XVisualInfo *visinfo = glXGetVisualFromFBConfig(display, fbconfig);
+	if (!visinfo) {
+		printf("Failed to get XVisualInfo\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("X Visual ID = 0x%.2x\n", (int)visinfo->visualid);
+	/* Create the X window */
+	XSetWindowAttributes winAttr ;
+	winAttr.event_mask = StructureNotifyMask | KeyPressMask ;
+	winAttr.background_pixmap = None ;
+	winAttr.background_pixel  = 0	 ;
+	winAttr.border_pixel	  = 0	 ;
+	winAttr.colormap = XCreateColormap(display, root_win,
+			visinfo->visual, AllocNone);
+	unsigned int mask = CWBackPixmap | CWBorderPixel | CWColormap | CWEventMask;
+	Window win = Win.win = XCreateWindow (display, root_win,
+			WIN_XPOS, WIN_YPOS,
+			WIN_XRES, WIN_YRES, 0,
+			visinfo->depth, InputOutput,
+			visinfo->visual, mask, &winAttr) ;
+	XStoreName(Win.display, win, "My GLX Window");
+	/* Create an OpenGL context and attach it to our X window */
+	GLXContext context = createContext(display, screen, fbconfig, visinfo, win);
+	if (! glXMakeCurrent(display, win, context))
+		printf("glXMakeCurrent failed.\n");
+	if (! glXIsDirect (display, glXGetCurrentContext()))
+		printf("Indirect GLX rendering context obtained\n");
+	/* Display the window */
+	XMapWindow(display, win);
+	if (! glXMakeCurrent(display, win, context))
+		printf("glXMakeCurrent failed.\n");
+	printf("Window Size    = %d x %d\n", WIN_XRES, WIN_YRES);
+	printf("Window Samples = %d\n", NUM_SAMPLES);
+}
 
-	/* Compile the fragment shader */
-	p = fragment_shader;
-	f = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(f, 1, &p, NULL);
-	glCompileShader(f);
-	glGetShaderInfoLog(f, sizeof msg, NULL, msg);
-	printf("fragment shader info: %s\n", msg);
+void processXEvents(Atom wm_protocols, Atom wm_delete_window) {
+	int setting_change = 0;
+	while (XEventsQueued(Win.display, QueuedAfterFlush)) {
+		XEvent event;
+		XNextEvent(Win.display, &event);
+		if(event.xany.window != Win.win)
+			continue;
+		switch (event.type) {
+			case MapNotify:
+				{
+					Win.displayed = 1;
+					break;
+				}
+			case ConfigureNotify:
+				{
+					XConfigureEvent cevent = event.xconfigure;
+					reshapeCB(cevent.width, cevent.height);
+					break;
+				}
+			case KeyPress:
+				{
+					char chr;
+					KeySym symbol;
+					XComposeStatus status;
+					XLookupString(&event.xkey, &chr, 1, &symbol, &status);
+					keyboardCB(symbol, chr, event.xkey.x, event.xkey.y,
+							&setting_change);
+					break;
+				}
+			case ClientMessage:
+				{
+					if (event.xclient.message_type == wm_protocols &&
+							(Atom)event.xclient.data.l[0] == wm_delete_window) {
+						exit(EXIT_SUCCESS);
+					}
+					break;
+				}
+		}
+	}
+}
 
-	/* Create and link the shader program */
-	program = glCreateProgram();
-	glAttachShader(program, v);
-	glAttachShader(program, f);
-	// Attribute location handling is simple enough for this app
-	// We just use manual values for the shader variables...
-	glBindAttribLocation(program, 0, "position");
-	//glBindAttribLocation(program, 1, "normal");
-	glBindAttribLocation(program, 2, "texcoord");
+void mainLoop() {
+	/* Register to receive window close events (the "X" window manager button) */
+	Atom wm_protocols	  = XInternAtom(Win.display, "WM_PROTOCOLS"    , False);
+	Atom wm_delete_window = XInternAtom(Win.display, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(Win.display, Win.win, &wm_delete_window, True);
 
-	glLinkProgram(program);
-	glGetProgramInfoLog(program, sizeof msg, NULL, msg);
-	printf("info: %s\n", msg);
+	while (1) {
+		/* Redraw window (after it's mapped) */
+		if (Win.displayed)
+			draw();
 
-	/* Enable the shaders */
-	glUseProgram(program);
+		/* Update frame rate */
+		struct timeval last_xcheck = {0, 0};
+		struct timeval now;
+		gettimeofday(&now, 0);
 
-	/* Get the locations of the uniforms so we can access them */
-	ModelViewProjectionMatrix_location = glGetUniformLocation(program, "ModelViewProjectionMatrix");
-	NormalMatrix_location = glGetUniformLocation(program, "NormalMatrix");
-	LightSourcePosition_location = glGetUniformLocation(program, "LightSourcePosition");
-	MaterialColor_location = glGetUniformLocation(program, "MaterialColor");
-	TextureSampler_location = glGetUniformLocation(program, "texSampler2D");
-	/* Set the LightSourcePosition uniform which is constant throught the program */
-	glUniform4fv(LightSourcePosition_location, 1, LightSourcePosition);
-
-	// Activate texture unit0 for basic texturing
-	glActiveTexture(GL_TEXTURE0);
+		/* Check X events every 1/10 second */
+		if (elapsedMsec(&last_xcheck, &now) > 100) {
+			processXEvents(wm_protocols, wm_delete_window);
+			last_xcheck = now;
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
-#ifdef TEST_MEMORYPROFILER_ALLOCATIONS_MAP
-	printf("You should see an interactive CPU profiler graph below, and below that an allocation map of the Emscripten main HEAP, with a long blue block of allocated memory.\n");
-#endif
-	printf("argc:%d\n", argc);
-	/* Initialize the window */
-	glutInit(&argc, argv);
-	glutInitWindowSize(win_width, win_heigth);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	Win.width = WIN_XRES;
+	Win.height = WIN_YRES;
+	createWindow();
 
-	glutCreateWindow("The fasttrack marker realtime tester " MCA_VERSION);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
 
-	/* Set up glut callback functions */
-	// This let us use "real" keyboard movements
-	// without this there is key repeat and the
-	// signals are being sent continously after
-	// the first signal + waiting and continous..
-	// Like in the terminal - which is bad as
-	// we can't record keydown/keyup pairs!
-	glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
-	glutIdleFunc(idle);
-	glutReshapeFunc(handleViewportReshape);
-	glutDisplayFunc(draw);
-	glutSpecialFunc(handleSpecialGlutEvents);
-	glutMouseFunc(handleMouseGlutEvents);
-
-	/* Do our initialization */
-	if(argc == 2) {
-		// User provided the model to load
-		init(argv[1]);
-	} else {
-		init(nullptr);
-	}
-
-	glutMainLoop();
-
-	return 0;
+	printf("Valid keys: Left, Right, k, ESC\n");
+	printf("Press ESC to quit\n");
+	mainLoop();
+	return EXIT_SUCCESS;
 }
-
 
 // vim: tabstop=4 noexpandtab shiftwidth=4 softtabstop=4
