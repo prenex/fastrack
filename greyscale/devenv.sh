f1e58b3468b798b96fc841816e6ab4e6c6fb0946
Richard Thier <u9vata@gmail.com>
2018-06-07 16:26:36 +0200

    Started to implement minimal 3D positioning POC - uses eigen and openvg for now (at least temporally) but the default make target does not rely on those things. For eigen on ubuntu, please use "sudo ln -s /usr/include/eigen3/Eigen/ /usr/include/Eigen" and grab submodules if you want to do a "make all" - which is not the default anymore!

--- greyscale/devenv.sh
+++ greyscale/devenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vim -p makefile microshackz.h marker1_gen.cpp fastforwardlist.h ffltest.cpp homer.h hoparser.h mcparser.h marker1_evaluator.cpp marker1_mc_evaluator.cpp marker_camapp.cpp v4lwrapper.h
+vim -p makefile microshackz.h marker1_gen.cpp fastforwardlist.h ffltest.cpp homer.h hoparser.h mcparser.h marker1_evaluator.cpp marker1_mc_evaluator.cpp marker_camapp.cpp v4lwrapper.h marker3d_camapp.cpp
